64cdf8292ce9632aa088d6b5c43e2fcca3ba4db2
patric noriega <patricnoriega@gmail.com>
2021-09-28 18:41:19 -0500

    feat: html-css complete starting js- global and DON variables setup

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,14 @@
+console.log("helloo");
+
+// global varible
+var userScore = 0;
+var cpuScore = 0;
+
+// DOM variables
+var userScore_span = document.getElementById('user-Score');
+var cpuScore_span = document.getElementById('cpu-score');
+var scoreBoard_div = document.querySelector(".score-board");
+var result_div = document.querySelector(".result");
+var rock_div = document.getElementById("rock");
+var paper_div = document.getElementById("paper");
+var scissors_div = document.getElementById("scissors");
