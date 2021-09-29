4443da579a691c2d743bfe0b8ea4317b61bd5c9f
patric noriega <patricnoriega@gmail.com>
2021-09-29 17:59:18 -0500

    game functionality works having trouble on scoreboard

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -8,44 +8,81 @@ var cpuScore = 0;
 var userScore_span = document.getElementById('user-Score');
 var cpuScore_span = document.getElementById('cpu-score');
 var scoreBoard_div = document.querySelector(".score-board");
-var result_div = document.querySelector('.result');
+var result_p = document.querySelector('.result > p');
 var rock_div = document.getElementById('rock');
 var paper_div = document.getElementById('paper');
 var scissors_div = document.getElementById('scissors');
 
 function getCpuChoice(){
-    var choices = ['rock', 'paper', 'scissors']
+    var choices = ['r', 'p', 's']
     var randomNumber = Math.floor(Math.random() * 3);
-
     return choices[randomNumber];
 }
-// console.log(getCpuChoice());
+console.log(getCpuChoice());
 
-function game(userChoice){                                              //console.logged to see click working..
-    // console.log("🤡  " + userChoice);
+function convertToWord(letter) {
+    if (letter === "r"){
+        return "Rock";
+    }  if (letter === "p"){
+        return "Paper";
+    }else {
+        return "Scissors"
+    }
+
+}
+
+function win(user, computer) {
+    for (var userScore = 0; userScore <= 1000; userScore++) {
+        userScore_span = userScore;
+        cpuScore_span = cpuScore;
+        result_p.innerHTML = convertToWord(user) + " beats " + convertToWord(computer) + " you win!";
+    }
+}
+function lose(){
+
+}
+function draw(){
+console.log("draw");
+}
+
+
+function game(userChoice) {
     var computerChoice = getCpuChoice();
     switch (userChoice + computerChoice) {
-        case "rock scissors":
-        case "paper rock":
-        case "scissors paper":
-            console.log('User Wins!!!');
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice, computerChoice);
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice, computerChoice);
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw(userChoice, computerChoice);
             break;
     }
 }
 
 
-rock_div.addEventListener("click", function () {            //console.logged to see click working..
-    // console.log('you clicked rock');
-    game('rock');
+function main() {
+    rock_div.addEventListener("click", function () {            //console.logged to see click working..
+        // console.log('you clicked rock');
+        game('r');
 
-});
+    })
 
-paper_div.addEventListener("click", function () {           //console.logged to see click working..
-    // console.log('you clicked paper');
-    game('paper');
-});
+    paper_div.addEventListener("click", function () {           //console.logged to see click working..
+        // console.log('you clicked paper');
+        game('p');
+    })
 
-scissors_div.addEventListener("click", function () {        //console.log to see click working..
-    // console.log('you clicked scissors');
-    game('scissors');
-});
+    scissors_div.addEventListener("click", function () {        //console.log to see click working..
+        // console.log('you clicked scissors');
+        game('s');
+    })
+}
+main();
