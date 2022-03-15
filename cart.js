4d48d328b91fad52b45fe9ee456abef3ae5378de
patric noriega <patricnoriega@gmail.com>
2022-03-15 12:37:17 -0500

    finished cart page styling is a bit off fix later

--- cart.js
+++ cart.js
@@ -95,7 +95,7 @@ function setItems(product) {
     localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
 
-function totalCost(product){
+function totalCost(product) {
     // console.log("the product price is ", product.price)
 
     let cartCost = localStorage.getItem("totalCost");
@@ -112,4 +112,45 @@ function totalCost(product){
 
 }
 
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart")
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".product")
+    let cartCost = localStorage.getItem("totalCost");
+
+    console.log(cartItems)
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="./img/${item.tag}.jpg" alt="item">
+                <span>${item.name}</span>
+                </div>
+            <div class="price">$${item.price}.00</div>
+            <div class="quantity">
+                <ion-icon class="decrease"
+                name="arrow-dropleft-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase"
+                name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price}.00      
+            </div>
+            `
+        });
+        productContainer.innerHTML += `
+           <div class="cartTotalContainer">
+                <h4 class="cartTotalTitle">Cart Total</h4>
+                <h4 class="cartTotal">$${cartCost}.00</h4>
+        
+        
+        `
+    }
+}
+
 onLoadCartNum();
+displayCart();
